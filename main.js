e8570b7a14d8be5145e11a3ae8c835418353213c
Srey1234 <74773452+Srey1234@users.noreply.github.com>
2021-08-04 18:37:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+noseX="";
+noseY="";
+GameStatus="";
+
+function startGame()
+{
+	GameStatus = "start";
+	document.getElementById("status").innerHTML = "Game is loading";
+}
+
+function game()
+{
+
+	console.log(" noseX = "  +  noseX + " noseY = "  +  noseY ) ;
+	    instializeInDraw();
+		moveEnvironment(mario);
+		drawSprites();
+}
+
 function preload() {
 	world_start = loadSound("world_start.wav");
 	setSprites();
@@ -9,13 +28,28 @@ function setup() {
     canvas.parent('canvas');
 
 	instializeInSetup(mario);
+
+	video = createCaputure(VIDEO);
+	video.size( 800 , 400 );
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video , modelLoaded );
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
 	game()
 }
 
-
+function gotPoses()
+{
+    if( results.length > 0 )
+    {
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.x;
+        console.log(" noseX = "  +  noseX + " noseY = "  +  noseY ) ;
+    }
+}
 
 
 
